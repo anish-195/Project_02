fd8c248fc97fe7ee897e91b6c176a98d30072501
Anish <anish.kumar344744@gmail.com>
2024-07-29 09:18:48 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let display = document.querySelector('#inputbox');
+let buttons = document.querySelectorAll('button');
+
+
+let buttonsArray = Array.from(buttons);
+let string = "";
+
+
+buttonsArray.forEach(btn=>{
+  btn.addEventListener('click',function(event){
+      
+   if(event.target.innerHTML == 'DEL'){
+    string = string.substring(0,string.length-1)
+    display.value = string;
+
+   }else if(event.target.innerHTML == 'AC'){
+         string = "";
+         display.value = string;
+
+   }else if(event.target.innerHTML == '='){
+        
+     string = eval(string);
+     display.value = string;
+   }else{
+    string += event.target.innerHTML; 
+    display.value = string;
+}
+
+  });
+});
+
+
